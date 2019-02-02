f459c2e1bd567e318d653682c88779ce8429b99b
Gaurav kedia <Gaurav.kedia18@gmail.com>
2019-02-03 02:16:52 +0530

    Receive Update Notification in notification bar when Available in Play Store.

--- app/src/main/java/com/gaurav/robotics_society/HomeActivity.java
+++ app/src/main/java/com/gaurav/robotics_society/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.gaurav.robotics_society;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AlertDialog;
@@ -13,6 +15,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.gaurav.robotics_society.Notify.NotificationHelper;
 import com.gaurav.robotics_society.app_update_checker.UpdateHelper;
 
 /**
@@ -20,28 +23,15 @@ import com.gaurav.robotics_society.app_update_checker.UpdateHelper;
  */
 
 public class HomeActivity extends AppCompatActivity implements UpdateHelper.onUpdateCheckListener {
+    private static final int NOTI_SECONDARY1 = 1201;
     private TextView btn1;
-
+    private NotificationHelper noti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
         getSupportActionBar().hide();
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.rslogo)
-                        .setContentTitle("Welcome")
-                        .setContentText("Welcome to the Society");
-
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(contentIntent);
-
-        // Add as notification
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
 
         UpdateHelper.with(this)
                 .onUpdateCheck(this)
@@ -69,28 +59,29 @@ public class HomeActivity extends AppCompatActivity implements UpdateHelper.onUp
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Toast.makeText(HomeActivity.this, "Updating!!!" + urlApp, Toast.LENGTH_SHORT).show();
                     }
-                }).setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
                 }).create();
         alertDialog.setCanceledOnTouchOutside(false);
         alertDialog.show();
 
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.rslogo)
-                        .setContentTitle("An Update is available")
-                        .setContentText("Please visit PlayStore to Update");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            noti = new NotificationHelper(this);
+            Notification.Builder nb = noti.getNotification2();
+            noti.notify(NOTI_SECONDARY1, nb);
+        } else {
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.rslogo)
+                            .setContentTitle("A Update is available")
+                            .setContentText("Please visit PlayStore to Update");
 
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(contentIntent);
+            Intent notificationIntent = new Intent(this, MainActivity.class);
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(contentIntent);
+            // Add as notification
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(0, builder.build());
+        }
 
-        // Add as notification
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
     }
 }
---
+++ app/src/main/java/com/gaurav/robotics_society/Notify/NotificationHelper.java
@@ -0,0 +1,86 @@
+package com.gaurav.robotics_society.Notify;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Color;
+import android.os.Build;
+
+import com.gaurav.robotics_society.R;
+
+/**
+ * Created by GAURAV on 03-02-2019.
+ */
+
+public class NotificationHelper extends ContextWrapper {
+
+    public static final String SECONDARY_CHANNEL = "Update";
+    private NotificationManager manager;
+
+    /**
+     * Registers notification channels, which can be used later by individual notifications.
+     *
+     * @param ctx The application context
+     */
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public NotificationHelper(Context ctx) {
+        super(ctx);
+
+        final NotificationChannel chan2 = new NotificationChannel(SECONDARY_CHANNEL,
+                "Updates", NotificationManager.IMPORTANCE_HIGH);
+        chan2.setLightColor(Color.BLUE);
+        chan2.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        getManager().createNotificationChannel(chan2);
+    }
+
+    /**
+     * Build notification for secondary channel.
+     *
+     * @return A Notification.Builder configured with the selected channel and details
+     */
+    @TargetApi(Build.VERSION_CODES.O)
+    public Notification.Builder getNotification2() {
+        return new Notification.Builder(getApplicationContext(), SECONDARY_CHANNEL)
+                .setContentTitle("Newer Version is Available")
+                .setContentText("Please Update to continue")
+                .setSmallIcon(getSmallIcon())
+                .setAutoCancel(true);
+    }
+
+    /**
+     * Send a notification.
+     *
+     * @param id           The ID of the notification
+     * @param notification The notification object
+     */
+    public void notify(int id, Notification.Builder notification) {
+        getManager().notify(id, notification.build());
+    }
+
+    /**
+     * Get the small icon for this app
+     *
+     * @return The small icon resource id
+     */
+    private int getSmallIcon() {
+        return R.drawable.rslogo;
+    }
+
+    /**
+     * Get the notification manager.
+     * <p>
+     * Utility method as this helper works with it a lot.
+     *
+     * @return The system service NotificationManager
+     */
+    private NotificationManager getManager() {
+        if (manager == null) {
+            manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return manager;
+    }
+}
